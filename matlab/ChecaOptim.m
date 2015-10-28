4bb0d129b41114894c7307543b1de88d9c552f9c
josmanps <jmps2812@gmail.com>
2015-10-28 14:49:30 -0600

    Selecciona los elementos que más violan la condición de optimalidad

--- matlab/ChecaOptim.m
+++ matlab/ChecaOptim.m
@@ -6,46 +6,38 @@ function [ optim,B ] = ChecaOptim( alfa,Y,g,rho,C,k,tol )
 % Creator : Joaquín Sánchez García
 % Modified by: José Manuel Proudinat Silva
 
-index1 = find( g-rho*Y > 0);
-index2 = find( g-rho*Y < 0);
+index1 = find( g-rho*Y > 0 );
+index2 = find( g-rho*Y < 0 );
 
-a = C*ones(length(index1),1)-alfa(index1);
+a = C - alfa(index1);
 b = alfa(index2);
 
 
 B = [];
 
-if norm( a,Inf) < tol && norm(b,Inf)< tol 
+if norm(a, Inf) < tol && norm(b, Inf)< tol 
     optim = 1;
     return
-else
-    
-    
+else    
     ind1 = a > tol;
     ind2 = b > tol;
+    a = a(ind1);
+    b = b(ind2);
     index1 = index1(ind1);
     index2 = index2(ind2); 
-    B = sort([index1;index2]);
+    B = [index1; index2];
     optim = 0;
 end
 
+% Get the elements that most violated the optimality condition
+violated = [a; b];
+[violated, order_B] = sort(violated, 'descend');
+violated
+B = B(order_B);
 
-
+% Get just k elements
 k = min(length(B), k);
-% We search for the coordinates for which the optimality condition is most
-% violated
-%aux = [a(index1) , b(index2)];
-%aux = sort(aux);
-%aux = aux(1:k);
-%[~,ind1] = ismember(a,aux);
-%[~,ind2] = ismember(b,aux);
-%ind1 (ind1 == 0) = [];
-%ind2 (ind2 == 0) = [];
-%index1 = index1(ind1);
-%index2 = index2(ind2);
-%B = sort([index1;index2])
-
- B = B(1:k);
+B = B(1:k);
 
 end
 
